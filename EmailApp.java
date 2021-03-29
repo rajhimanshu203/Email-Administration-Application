8cbe1c777ca6307d30a9dd20b3a3eb3e2dd86f70
Rajhimanshu203 <rajhimanshu203@gmail.com>
2021-03-29 19:49:12 +0530

    first commit

---
+++ EmailApp.java
@@ -0,0 +1,14 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Email em1 = new Email("Himanshu", "Raj");
+		//em1.setAlternateEmail("rajhiman@gmail.com");
+		//System.out.println(em1.getAlternateEmail());
+          
+		System.out.println(em1.showInfo());
+	}
+
+}
